f106a25637eda7dfa62ccc892e612a42fa2bc69f
Yulder A. Avila <77408569+Y-Avila@users.noreply.github.com>
2021-12-10 15:19:39 -0500

    PublicarSubModulo

---
+++ SubModulo/navigation/LinkingConfiguration.ts
@@ -0,0 +1,35 @@
+/**
+ * Learn more about deep linking with React Navigation
+ * https://reactnavigation.org/docs/deep-linking
+ * https://reactnavigation.org/docs/configuring-links
+ */
+
+import { LinkingOptions } from '@react-navigation/native';
+import * as Linking from 'expo-linking';
+
+import { RootStackParamList } from '../types';
+
+const linking: LinkingOptions<RootStackParamList> = {
+  prefixes: [Linking.makeUrl('/')],
+  config: {
+    screens: {
+      Root: {
+        screens: {
+          Inicio:{ screens:{ Inicio: 'inicio',}, },
+          
+          SignUp:{ screens:{ SignUp: 'SignUp'},  },
+          
+          VerProyectos:{ screens:{ VerProyectos: 'verProyectos'},  }, 
+          
+          VerInscripciones:{ screens:{ VerInscripciones: 'VerInscripciones'},  },    
+          
+          VerAvances:{ screens:{ VerAvances: 'VerAvances'},  },           
+        },  
+      },        
+      Modal: 'modal',
+      NotFound: '*',
+    },
+  },
+};
+
+export default linking;
---
+++ SubModulo/types.tsx
@@ -0,0 +1,62 @@
+/**
+ * Learn more about using TypeScript with React Navigation:
+ * https://reactnavigation.org/docs/typescript/
+ */
+
+import { BottomTabScreenProps } from '@react-navigation/bottom-tabs';
+import { CompositeScreenProps, NavigatorScreenParams } from '@react-navigation/native';
+import { NativeStackScreenProps } from '@react-navigation/native-stack';
+
+declare global {
+  namespace ReactNavigation {
+    interface RootParamList extends RootStackParamList {}
+  }
+}
+
+export type RootStackParamList = {
+  Root: NavigatorScreenParams<RootTabParamList> | undefined;
+  Modal: undefined;
+  NotFound: undefined;
+};
+
+export type RootStackScreenProps<Screen extends keyof RootStackParamList> = NativeStackScreenProps<
+  RootStackParamList,
+  Screen
+>;
+
+export type RootTabParamList = {
+  
+  
+  //General
+  Inicio: undefined;
+  SignUp: undefined;
+  SignIn: undefined;
+
+  //Usuarios
+  ActualizarEstado: undefined;
+  ActualizarPerfil: undefined;
+
+  //Proyectos
+  VerProyectos: undefined;
+  verDetallesProyecto: undefined;
+  RegistrarProyecto: undefined;
+  ActualizarProyecto: undefined;
+
+
+  //Inscripciones
+  CrearInscripcion: undefined;
+  VerInscripciones: undefined;
+  ActualizarEstadoInscripciones: undefined;
+
+  //Avances
+  CrearAvance: undefined;
+  VerAvances: undefined;
+  ActualizarAvances: undefined;
+
+  
+};
+
+export type RootTabScreenProps<Screen extends keyof RootTabParamList> =  
+  CompositeScreenProps<BottomTabScreenProps<RootTabParamList, Screen>,
+  NativeStackScreenProps<RootStackParamList>
+>;
